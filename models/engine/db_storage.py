e83c0c83cd4ab5eff17dfdef125e87116cb39afc
root <root@SupremePrime-Rick.localdomain>
2023-02-21 09:45:46 -0600

    done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -51,21 +51,6 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
-    def get(self, cls, id):
-        """ Returns the object based on the class"""
-        """ and its ID, or None if not found"""
-        obj = None
-        if cls is not None and issubclass(cls, BaseModel):
-            obj = self.__session.query(cls).filter(cls.id == id).first()
-        return obj
-
-    def count(self, cls=None):
-        """Returns the number of objects in storage """
-        """matching the given class."""
-        """ If no class is passed """
-        """ returns the count of all objects in storage"""
-        return len(self.all(cls))
-
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
@@ -89,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ retrieves """
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ counts """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
